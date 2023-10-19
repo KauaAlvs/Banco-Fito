fcdd659f69cf75af077b63f5eb4b18d865437b8b
Kaua Alves <134338789+KauaAlvs@users.noreply.github.com>
2023-10-19 09:41:24 -0300

    Add files via upload

---
+++ BANCO FETO/PHP1/contas.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contas Bancárias - BancoFácil</title>
+    <link rel="stylesheet" href="http://localhost/CSS/style.css">
+</head>
+<body>
+    <header>
+        <h1>BancoFácil - Contas Bancárias</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="http://localhost/PHP1/contas.php">Contas Bancárias</a></li>
+            <li><a href="http://localhost/PHP2/cartoes.php">Cartões de Crédito</a></li>
+            <li><a href="http://localhost/PHP3/investimentos.php">Investimentos</a></li>
+            <li><a href="http://localhost/PHP4/emprestimos.php">Empréstimos</a></li>
+            <li><a href="http://localhost/index.html">Menu</a></li>
+        </ul>
+    </nav>
+    <main>
+    </main>
+    <footer>
+        <p>&copy; 2023 Banco Fito</p>
+    </footer>
+</body>
+</html>
